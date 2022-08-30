0daec57028737a98d867bfaa6b54a240d024ab7f
shoaibejaz <31452309+shoaibejaz@users.noreply.github.com>
2022-08-30 13:08:35 +0500

    pdf view and download work complete

--- src/app/dashboardLayout/components/pdf-popup/pdf-popup.component.ts
+++ src/app/dashboardLayout/components/pdf-popup/pdf-popup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-pdf-popup',
@@ -8,11 +9,24 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class PdfPopupComponent implements OnInit {
 
-  constructor( private dialogRef: MatDialogRef<PdfPopupComponent> ) { }
+  file: string = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
+
+  constructor( 
+    private dialogRef: MatDialogRef<PdfPopupComponent>,
+  ) { }
+
+  downloadPDF( fileURL: string ) {
+    const fileName = fileURL.split( '/' ).pop();
+    saveAs( fileURL, fileName );
+  };
+
+  openPDF( fileURL: string ) {
+    window.open( fileURL );
+  };
 
   closeDialog() {
     this.dialogRef.close();
-  }
+  };
 
   ngOnInit(): void {
   }
